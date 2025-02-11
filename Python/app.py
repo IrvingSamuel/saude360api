6fbd5551c8c04868dbab4afda2f5503f0560e4ba
Irving Samuel <sync.irvingsamuel@gmail.com>
2025-02-10 23:29:49 -0300

    Commit inicial

--- Python/app.py
+++ Python/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.main import main_bp
 import config
+import os
 
 app = Flask(__name__)
 app.config.from_object(config.Config)
@@ -9,4 +10,5 @@ app.config.from_object(config.Config)
 app.register_blueprint(main_bp, url_prefix="/api")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Pega a porta definida pelo Railway
+    app.run(host="0.0.0.0", port=port)
