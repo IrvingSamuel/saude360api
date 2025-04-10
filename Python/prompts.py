597b4425369d1c67315998e4016c7075c06534c8
Irving Samuel <sync.irvingsamuel@gmail.com>
2025-04-10 07:57:25 -0300

    atualização de troca para json

--- Python/prompts.py
+++ Python/prompts.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 client = OpenAI()
 
-# Example HTML template
+# exemplo html para resposta
 html_template = """
 <div class="card" style="width: 18rem;">
     <div class="card-body">
@@ -12,7 +12,7 @@ html_template = """
 </div>
 """
 
-# List of reference websites
+# Lista de sites para referência textual
 reference_websites = [
     "https://epocanegocios.globo.com/inteligencia-artificial/noticia/2023/12/conheca-15-especialistas-em-inteligencia-artificial-empresarial-que-se-destacaram-em-2023.ghtml ",
     "https://abeso.org.br/conceitos/obesidade-e-sindrome-metabolica/",
@@ -24,27 +24,27 @@ def process(prompt):
         completion = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": """Comporte-se como um sistema que vai retornar extritamente respostas html 
-                 que serão introduzidas em um sistema de CRUD de pacientes para um usuário médico, com o intuito de ajudar a 
-                 diagnosticar a sindrome metabólica (Doenças hereditárias), as informações vão 
-                 ser passadas como texto, e o sistema vai retornar um 
-                 texto com a resposta, contendo apenas: Considerações clinicas, Recomendações e conclusão.
-                 Como um resultado final indique em porcentagem, o mais aproximada possível, o risco de se desenvolver cada tipo de sindrome metabólica no paciente 
-                 O texto retornado vai vir como um html que vai ser introduzido dentro de um card.
-                 Utilize os seguintes sites como 
-                 fonte de consulta: """ + ', '.join(reference_websites).join("""
-                , Template de uso estrito para resposta a ser usado:
-                Considerações Clínicas
-                O paciente --, -- anos, apresenta IMC ---- para sua altura e peso. Os sinais vitais estão ----, com glicose ----. É importante notar que o RCQ ----.
+                {"role": "system", "content": """Comporte-se como um sistema que vai retornar extritamente um json com indices prefefinidos,
+                    você vai receber um texto com informações sobre um paciente que serão introduzidas em um sistema de CRUD de pacientes para um usuário médico, com o intuito de ajudar a 
+                    diagnosticar a sindrome metabólica (Doenças hereditárias), as informações vão 
+                    ser passadas como texto, e o sistema vai retornar um 
+                    json com a resposta com os valores dos indices também sendo texto, contendo apenas as chaves: consideracoes_clinicas, recomendacoes_medicas, conclusao_do_caso e risco_de_sm (risco de sindrome metabólica em %).
+                    para o risco de desenvolver sindrome metabólica, indique em porcentagem, o mais aproximada possível.
+                    Utilize os seguintes sites como 
+                    fonte de consulta: """ + ', '.join(reference_websites).join("""
+                    , Template exemplificativo de uso estrito para resposta a ser usado:
+                    consideracoes_clinicas:
+                    O paciente --, -- anos, apresenta IMC ---- para sua altura e peso. Os sinais vitais estão ----, com glicose ----. É importante notar que o RCQ ----.
 
-                Recomendações
-                Recomenda-se que o paciente adote ----.
+                    recomendacoes_medicas:
+                    Recomenda-se que o paciente adote ----.
 
-                Conclusão
-                Com os dados apresentados, -- possui um -- risco de desenvolver diabetes tipo 2, e sua pressão arterial está --. Recomendo ---.
-
-                Risco aproximado de desenvolver Síndrome Metabólica: --%
-                """)
+                    conclusao_do_caso:
+                    Com os dados apresentados, -- possui um -- risco de desenvolver diabetes tipo 2, e sua pressão arterial está --. Recomendo ---.
+                                                                                
+                    risco_de_sm:
+                    --%
+                    """)
                  },
                 {
                     "role": "user",
